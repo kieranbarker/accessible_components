55045895a9092f10f9509fddf897c8258f0b0e22
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-01-08 12:22:36 +0000

    Refactor menu script

--- menu_and_menu_button/app.js
+++ menu_and_menu_button/app.js
@@ -60,49 +60,33 @@ function handleClick(event) {
 }
 
 /**
- * Handle the arrow down key when the menu button is in focus.
+ * Handle the arrow keys for the menu button.
+ * @param {boolean} isArrowUp
+ * @param {boolean} isArrowDown
  */
-function handleArrowDownKey() {
-  // If the menu is already open, do nothing
+function handleMenuButton(isArrowUp, isArrowDown) {
+  // Get the expanded state
   const isExpanded = menuButton.getAttribute('aria-expanded');
-  if (isExpanded !== 'false') return;
 
-  // Otherwise, open the menu
-  show();
-  menuItems[0].focus();
-}
+  // If the menu is closed, open it
+  if (isExpanded === 'false') {
+    show();
+  }
 
-/**
- * Handle the escape key when the menu button or a menu item is in focus.
- */
-function handleEscapeKey() {
-  hide();
-  menuButton.focus();
-}
-
-/**
- * Handle the menu button when it's in focus.
- * @param {Event} event
- */
-function handleMenuButton(event) {
-  // Check which key was pressed
-  const isArrowDown = event.key === 'ArrowDown' || event.key === 'Down';
-  const isEscape = event.key === 'Escape' || event.key === 'Esc';
-
-  // Handle arrow down and escape keys
-  if (isArrowDown) {
-    handleArrowDownKey();
-  } else if (isEscape) {
-    handleEscapeKey();
+  // Shift focus to the last/first item
+  if (isArrowUp) {
+    menuItems[menuItems.length - 1].focus();
+  } else if (isArrowDown) {
+    menuItems[0].focus();
   }
 }
 
 /**
- * Handle the arrow keys when a menu item is in focus.
+ * Handle the arrow keys for a menu item.
  * @param {boolean} isArrowUp
  * @param {boolean} isArrowDown
  */
-function handleArrowKeys(isArrowUp, isArrowDown) {
+function handleMenuItem(isArrowUp, isArrowDown) {
   // Find the index of the item that's in focus
   const index = menuItems.findIndex(item => {
     return item === document.activeElement;
@@ -119,37 +103,50 @@ function handleArrowKeys(isArrowUp, isArrowDown) {
 }
 
 /**
- * Handle a menu item when it's in focus.
- * @param {Event} event
+ * Handle the arrow keys.
+ * @param {boolean} isArrowUp
+ * @param {boolean} isArrowDown
  */
-function handleMenuItem(event) {
+function handleArrowKeys(isArrowUp, isArrowDown) {
+  // Check which element is in focus
+  const isMenuButtonInFocus = document.activeElement.matches('#menu-button');
+  const isMenuItemInFocus = document.activeElement.matches('#menu a');
+
+  // Handle the menu button and menu items
+  if (isMenuButtonInFocus) {
+    handleMenuButton(isArrowUp, isArrowDown);
+  } else if (isMenuItemInFocus) {
+    handleMenuItem(isArrowUp, isArrowDown);
+  }
+}
+
+/**
+ * Handle the escape key.
+ */
+function handleEscapeKey() {
+  // Check if a menu item is in focus
+  const isMenuItemInFocus = document.activeElement.matches('#menu a');
+
+  // Close the menu
+  hide();
+
+  // If a menu item is in focus, shift focus to the menu button
+  if (isMenuItemInFocus) {
+    menuButton.focus();
+  }
+}
+
+function handleKeydown(event) {
   // Check which key was pressed
   const isArrowUp = event.key === 'ArrowUp' || event.key === 'Up';
   const isArrowDown = event.key === 'ArrowDown' || event.key === 'Down';
   const isEscape = event.key === 'Escape' || event.key === 'Esc';
 
-  // Handle arrow keys and escape key
+  // Handle the arrow and escape keys
   if (isArrowUp || isArrowDown) {
     handleArrowKeys(isArrowUp, isArrowDown);
   } else if (isEscape) {
     handleEscapeKey();
-  }
-}
-
-/**
- * Handle keydown events.
- * @param {Event} event
- */
-function handleKeydown(event) {
-  // Check which element is in focus
-  const isMenuButtonInFocus = document.activeElement.matches('#menu-button');
-  const isMenuItemInFocus = document.activeElement.matches('#menu a');
-
-  // Handle menu button and menu items
-  if (isMenuButtonInFocus) {
-    handleMenuButton(event);
-  } else if (isMenuItemInFocus) {
-    handleMenuItem(event);
   }
 }
 
