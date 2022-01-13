97e00792ad7a88961e075ac653c9d3e77be845f6
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-01-13 14:35:30 +0000

    Add JSDoc to menu component functions

--- menu_and_menu_button/app.js
+++ menu_and_menu_button/app.js
@@ -5,24 +5,40 @@ const button = main.querySelector('#menu-button');
 const menu = main.querySelector('#menu');
 const menuItems = [ ...menu.querySelectorAll('[role="menuitem"]') ];
 
+/**
+ * Show the menu.
+ */
 function show() {
   menu.removeAttribute('hidden');
   button.setAttribute('aria-expanded', 'true');
 }
 
+/**
+ * Hide the menu.
+ */
 function hide() {
   menu.setAttribute('hidden', '');
   button.removeAttribute('aria-expanded');
 }
 
+/**
+ * Shift focus to the first menu item.
+ */
 function focusFirstItem() {
   menuItems[0].focus();
 }
 
+/**
+ * Shift focus to the last menu item.
+ */
 function focusLastItem() {
   menuItems[menuItems.length - 1].focus();
 }
 
+/**
+ * Handle keydown events on the button.
+ * @param {KeyboardEvent} event
+ */
 function handleButtonKeydown(event) {
   let shouldPreventDefault = false;
 
@@ -48,10 +64,17 @@ function handleButtonKeydown(event) {
   }
 }
 
+/**
+ * Check if the menu is expanded.
+ * @returns {boolean}
+ */
 function isExpanded() {
   return button.getAttribute('aria-expanded') === 'true';
 }
 
+/**
+ * Handle click events on the button.
+ */
 function handleButtonClick() {
   if (isExpanded()) {
     hide();
@@ -62,22 +85,36 @@ function handleButtonClick() {
   }
 }
 
+/**
+ * Find the index of the menu item in focus.
+ * @returns {number}
+ */
 function findIndexInFocus() {
   return menuItems.findIndex(item => item === document.activeElement);
 }
 
+/**
+ * Shift focus to the next menu item.
+ */
 function focusNextItem() {
   const indexInFocus = findIndexInFocus();
   const nextItem = menuItems[indexInFocus + 1];
   nextItem ? nextItem.focus() : focusFirstItem();
 }
 
+/**
+ * Shift focus to the previous menu item.
+ */
 function focusPrevItem() {
   const indexInFocus = findIndexInFocus();
   const prevItem = menuItems[indexInFocus - 1];
   prevItem ? prevItem.focus() : focusLastItem();
 }
 
+/**
+ * Handle keydown events on the menu items.
+ * @param {KeyboardEvent} event
+ */
 function handleItemKeydown(event) {
   const isFirstItem = event.target === menuItems[0];
   const isLastItem = event.target === menuItems[menuItems.length - 1];
@@ -131,10 +168,18 @@ function handleItemKeydown(event) {
   }
 }
 
+/**
+ * Handle mouseover events on the menu items.
+ * @param {MouseEvent} event
+ */
 function handleItemMouseover(event) {
   event.target.focus();
 }
 
+/**
+ * Handle mousedown events outside the component.
+ * @param {MouseEvent} event
+ */
 function handleMousedown(event) {
   if (main.contains(event.target)) return;
   if (!isExpanded()) return;
